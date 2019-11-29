2c468551bc46431427b733fa35291563d0e0a49f
Novoseltsev Nikita <pofeozzz@gmail.com>
2019-11-29 18:04:11 +0700

    5 практика

--- 747-nni-5-2.c
+++ 747-nni-5-2.c
@@ -51,7 +51,7 @@ int turnLeft(Tree* tree);
 
 int turnRight(Tree *tree) ;
 
-void print(TempNode* node);
+void print(Tree* Root);
 
 void printTree(Tree *tree) ;
 
@@ -63,47 +63,17 @@ int main() {
 	Tree *tree = (Tree*)malloc(sizeof(TempNode));
 	init(tree);
 
-	int a[4];
-	scanf("%d%d%d%d", &a[0], &a[1], &a[2], &a[3]);
+	int a[7];
+	scanf("%d%d%d%d%d%d%d", &a[0], &a[1], &a[2], &a[3] ,&a[4],&a[5],&a[6]);
 		insert(tree, a[0]);
 		insert(tree, a[1]);
 		insert(tree, a[2]);
 		insert(tree, a[3]);
-	print(tree->root);
-
-	scanf("%d%d%d", &a[0], &a[1], &a[2]);
-		insert(tree, a[0]);
-		insert(tree, a[1]);
-		insert(tree, a[2]);
-
-	print(tree->root);
-
-int m[3];
-	scanf("%d", &m[0]);
-CoutParentandSon(m[0],tree);
-
-	scanf("%d", &m[1]);
-CoutParentandSon(m[1],tree);
-
-
-	scanf("%d", &m[2]);
-
-	Remove(tree, m[2]);
-
-	print(tree->root);
-
-	while (turnLeft(tree) == 0);
-
-	print(tree->root);
-	while (turnRight(tree) == 0);
-
-	print(tree->root);
-
-	printf("%d\n", tree->count);
-
-	clear(tree);
-
-	print(tree->root);
+		insert(tree, a[4]);
+		insert(tree, a[5]);
+		insert(tree, a[6]);
+	print(tree);
+	printf("\n");
 }
 
 void createlist(List *r_)
@@ -299,84 +269,34 @@ int turnRight(Tree *tree) {
 
 	return 0;
 }
-void print(TempNode* node)
+void print(Tree* Root)
 {
-	if (node)
+	TempNode *mass[100];
+	TempNode *mass2[100];
+	int TmpForMass =0;
+	int TmpForMass2 =0;
+	TempNode * TempStruct = Root -> root;
+	while(TmpForMass2 <Root ->count)
 	{
-		List ListTmp, List_;
-		List *GigListTmp = &ListTmp;
-		List *GigList_ = &List_;
-		createlist(GigListTmp);
-		createlist(GigList_);
-		TempNode *TmpParam = NULL;
-		TaskList*r_ = NULL;
-		int c = 0, val = node->value, j = 0;
-		TempNode *l = node->left;
-		TempNode *r = node->right;
-		input(GigListTmp, node);
-		while (1)
+		while (TempStruct!= NULL)
 		{
-			while (GigListTmp->first)
-			{
-				if (GigListTmp->first)
-	{
-		TaskList *nodelist_pop;
-		TmpParam = GigListTmp->first->Gig_node_tree;
-		nodelist_pop = GigListTmp->first;
-		GigListTmp->first = GigListTmp->first->next;
-		if (GigListTmp->first == NULL)
-			GigListTmp->last = NULL;
-		free(nodelist_pop);
-	}
-	else
-		TmpParam = NULL;
-				if (GigListTmp->first == NULL)
-				{
-					if (TmpParam->value == val && c == 1)
-						printf("_\n");
-					else
-						printf("%d\n", TmpParam->value);
-				}
-				else if (TmpParam->value == val && c == 1)
-					printf("_ ");
-				else
-					printf("%d ", TmpParam->value);
-				if (TmpParam->left)
-					input(GigList_, TmpParam->left);
-				else
-					input(GigList_, node);
-				if (TmpParam->right)
-					input(GigList_, TmpParam->right);
-				else
-					input(GigList_, node);
+			TmpForMass2 = TmpForMass2 + 1;
+			if(TempStruct -> right != NULL)
+			{TmpForMass = TmpForMass + 1 ;
+				mass[TmpForMass] = TempStruct->right;
 			}
-			node->left = node->right = NULL;
-			c = 1;
-			r_ = GigList_->first;
-			while (GigList_->first->Gig_node_tree->value == val)
-			{
-				GigList_->first = GigList_->first->next;
-				if (GigList_->first->next == NULL)
-					if (GigList_->first->Gig_node_tree->value == val)
-					{
-						j = 1;
-						node->left = l;
-						node->right = r;
-						clearlist(GigList_);
-						break;
-					}
-			}
-			GigList_->first = r_;
-			if (j == 1)
-				break;
-			GigListTmp->first = GigList_->first;
-			GigListTmp->last = GigList_->last;
-			GigList_->first = NULL;
-			GigList_->last = NULL;
+			mass2[TmpForMass2] = TempStruct;
+			TempStruct = TempStruct->left;
+			
 		}
+		TempStruct = mass[TmpForMass];
+		TmpForMass = TmpForMass - 1 ;
 	}
-	else
-		printf("-\n");
+	for (int i = 1 ;i <= TmpForMass2;i++)
+	{
+	    mass2[i] = mass2[i] -> value;
+		printf("%d ",mass2[i]);
+}
 }
 void printTree(Tree *tree) {
 	print(tree->root);
